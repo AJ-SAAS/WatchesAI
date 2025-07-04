f408ee08ffb0a1f04e56a3043be9f4797fe886c5
Ajay Sharma <gridking111@gmail.com>
2025-07-04 10:36:11 +0300

    Initial Commit

---
+++ WatchesAI/WatchesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchesAIApp.swift
+//  WatchesAI
+//
+//  Created by Work Laptop on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchesAITests/WatchesAITests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchesAITests.swift
+//  WatchesAITests
+//
+//  Created by Work Laptop on 04/07/2025.
+//
+
+import Testing
+@testable import WatchesAI
+
+struct WatchesAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
